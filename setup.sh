734a2473783a90d7363ea45efa606dbc3e647c24
Brian McDaniel <brianmcd05@gmail.com>
2019-07-20 07:57:23 -0400

    Force symlink update in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Setup git
-ln -s `pwd`/.gitconfig $HOME/.gitconfig
-ln -s `pwd`/.gitignore_global $HOME/.gitignore_global
+ln -sf `pwd`/.gitconfig $HOME/.gitconfig
+ln -sf `pwd`/.gitignore_global $HOME/.gitignore_global
 
 # Setup vim
-ln -s `pwd`/.vimrc $HOME/.vimrc
-ln -s `pwd`/init.vim $HOME/.config/nvim/init.vim
-ln -s `pwd`/coc.nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json
+ln -sf `pwd`/.vimrc $HOME/.vimrc
+ln -sf `pwd`/init.vim $HOME/.config/nvim/init.vim
+ln -sf `pwd`/coc.nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json
 mkdir -p $HOME/.config/coc/extensions
-ln -s `pwd`/coc.nvim/extensions/package.json $HOME/.config/coc/extensions/package.json
-ln -s `pwd`/coc.nvim/extensions/yarn.lock $HOME/.config/coc/extensions/yarn.lock
+ln -sf `pwd`/coc.nvim/extensions/package.json $HOME/.config/coc/extensions/package.json
+ln -sf `pwd`/coc.nvim/extensions/yarn.lock $HOME/.config/coc/extensions/yarn.lock
 
-ln -s `pwd`/.tmux.conf $HOME/.tmux.conf
+ln -sf `pwd`/.tmux.conf $HOME/.tmux.conf
 
-ln -s `pwd`/i3 $HOME/.config/i3/config
+ln -sf `pwd`/i3 $HOME/.config/i3/config
 
-ln -s `pwd`/.bashrc-custom $HOME/.bashrc-custom
+ln -sf `pwd`/.bashrc-custom $HOME/.bashrc-custom
 echo "source $HOME/.bashrc-custom" >> $HOME/.bashrc
